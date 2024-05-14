404c4e3e3e01a93d24637002667a1c75f45abcdd
asmaa-dreissia <asmaa.dreissia@laplateforme.io>
2024-05-14 16:01:34 +0200

    Etape 2 terminé

--- jour2/index.js
+++ jour2/index.js
@@ -1,4 +1,8 @@
-const server = require('./server');
+const http = require('http');
+const handleRoutes = require('./routes');
+
+// Créer le serveur HTTP
+const server = http.createServer(handleRoutes);
 
 // Définir le port sur lequel le serveur écoutera
 const PORT = process.env.PORT || 3000;
